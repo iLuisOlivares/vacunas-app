3c57ce944c109d76e8cd4b2d5af9ce2cb711e884
Sebastian <luissolivaresp@gmail.com>
2023-09-14 17:17:36 -0500

    💄 Update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,13 +4,23 @@ const Navbar = () => {
     return (
 
         <>
-            <nav className="bg-slate-100 py-5 px-20 items-center flex justify-between mb-4 ">
-                <Link href={"/"} > <h1 className="text-3xl font-bold">VacunasApp</h1 > </Link>
+            <nav className="bg-white10 py-4 px-10 items-center flex justify-between mb-4  ">
+                <Link href={"/"} >
+                    <div className="">
+                        <h1 className="flex items-end    text-3xl font-bold border-double rounded-full   
+                        border-y-darkseagreen border-x-sky-800 text-darkseagreen300 border-8 p-2">
+                            VanPet
+                            <img className="h-10" src="https://res.cloudinary.com/iluiss/image/upload/v1694729473/Recuerdos%20Lucas/v20oxqeyvw5i11cbblhc.png" alt="" />
+                        </h1>
+                    </div>
+                </Link>
+
                 <ul className="flex gap-2">
                     <li>
                         <Link href="/">home</Link>
                     </li>
                     <li>
+
                         <Link href="/mascotas">mascotas</Link>
                     </li>
                     <li>
